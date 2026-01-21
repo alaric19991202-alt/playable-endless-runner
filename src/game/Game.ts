9f030d83feb1650ae587e12d7b453be1e9f6a5e0
helios <alaric19991202@gmail.com>
2026-01-21 14:29:40 +0100

    feat: create PIXI app setup and bundler entry with game orchestrator

---
+++ src/game/Game.ts
@@ -0,0 +1,15 @@
+import * as PIXI from "pixi.js";
+
+export class Game {
+  app: PIXI.Application;
+  private ctaUrl?: string;
+
+  constructor(private mount: HTMLElement) {
+
+  }
+
+  async start() {
+
+  }
+
+}
